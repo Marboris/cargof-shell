adc6b1832c2925fc21432b690e35768e932ee9bb
MehraB832 <mehrabshafae@gmail.com>
2024-09-14 14:19:08 +0330

    fix

--- cargof.sh
+++ cargof.sh
@@ -1,52 +1,63 @@
 #!/bin/bash
 
-# بررسی ورودی
 if [ "$1" != "build" ] && [ "$1" != "new" ]; then
-  echo "Usage: $0 {build|new} [name]"
+  echo "Usage: $0 {build|new} [options]"
   exit 1
 fi
 
-# تابع برای اجرای build
 build() {
-  # اجرای cargo build
   cargo build --release
-  
-  # استخراج نام پکیج از فایل Cargo.toml
+
   package_name=$(grep '^name =' Cargo.toml | sed -E 's/.*name\s*=\s*"(.*)".*/\1/')
-  
-  # بررسی استخراج نام پکیج
+
   if [ -z "$package_name" ]; then
     echo "Could not extract package name from Cargo.toml"
     exit 1
   fi
-  
-  # صبر کردن به مدت 1 ثانیه
-  sleep 1
-  
-  # ایجاد دایرکتوری release
+
+  sleep 0.5
+
   mkdir -p ./release
-  
-  # کپی فایل باینری به دایرکتوری release
+
+  sleep 0.5
+
   cp ./target/release/"$package_name" ./release/
-  
-  # تغییر نام فایل باینری
-  processor_model=$(uname -m)  # گرفتن مدل پردازنده
+
+  sleep 0.5
+
+  processor_model=$(uname -m)
   mv ./release/"$package_name" ./release/"$package_name"_"$processor_model"
-  
-  # چاپ اندازه فایل نهایی
+
   du -sh ./release/"$package_name"_"$processor_model"
 }
 
-# تابع برای اجرای new
 new() {
-  if [ -z "$2" ]; then
-    echo "Usage: $0 new name"
+  if [[ "$2" != n=* ]]; then
+    echo "Usage: $0 new n=Name [other-options]"
     exit 1
   fi
-  echo "New name is: $2"
-}
 
-# اجرای تابع مربوط به ورودی
+  name=$(echo "$2" | sed -E 's/^n=//')
+
+  shift 2
+  additional_options="$@"
+
+  cargo new "$name" $additional_options
+
+  sleep 0.5
+
+  cd "$name" || exit
+
+  cat <<EOL >>Cargo.toml
+
+[profile.release]
+opt-level = 3
+lto = true
+codegen-units = 1
+EOL
+
+  echo "Project $name created and profile settings added."
+}
 if [ "$1" == "build" ]; then
   build
 elif [ "$1" == "new" ]; then
